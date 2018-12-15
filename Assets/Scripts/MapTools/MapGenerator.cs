db0187ef18b852effb373688bdac0506355d1d14
emerson.silva <email.emerson.silva@gmail.com>
2018-12-15 12:02:48 -0800

    TileMap
    Scripts iniciais de geracao de mapa.

---
+++ Assets/Scripts/MapTools/MapGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	public Vector2 mapSize;
+	public Transform tilePrefab;
+	public ArrayList tiles = new ArrayList();
+
+	// Use this for initialization
+	void Start ()
+	{
+		GenerateMap();
+	}
+
+	private void GenerateMap()
+	{
+		Debug.Log("Generating Map");
+		for (int x=0 ; x<mapSize.x; x++) {
+			for (int y=0; y<mapSize.y; y++) {
+				Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, -mapSize.y/2 + 0.5f + y, 0);
+				Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.identity) as Transform;
+				tiles.Add(newTile);
+			}
+		}
+		Debug.Log("Generating Map");
+	}
+	
+}
