39e19e79ddebe77396fdc712b2174a8f75db4a81
emerson.silva <email.emerson.silva@gmail.com>
2019-01-27 12:18:06 -0800

    MapGenerator
    MapGenerator e prefabs para dev

--- Assets/Scripts/MapTools/MapGenerator.cs
+++ Assets/Scripts/MapTools/MapGenerator.cs
@@ -5,25 +5,59 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
+	public const int TILE_COMMON_GROUND = 0;
+	public const int TILE_COMMON_WALL = 1;
+	public const int TILE_DESTROYABLE_WALL = 2;
+	public const int TILE_SPAWN_PLAYER = 3;
+	public const int TILE_SPAWN_COMMON_ENEMY = 4;
+	public const int TILE_SPAWN_ROGUE_ENEMY = 5;
+	public const int TILE_SPAWN_MAGE_ENEMY = 6;
+	public const int TILE_SPAWN_WARRIOR_ENEMY = 7;
+	public const int TILE_NEXT_FASE = 8;
+
 	public Vector2 mapSize;
 	public ArrayList tiles = new ArrayList();
 
-	public Transform communGround;
-
 	public GameObject[] walls;
 
+	public Transform communGround;
+	public Transform communWall;
+	public Transform destroyableWall;
+
+	public Transform player;
+	public Transform commonEnemy;
+	public Transform rogueEnemy;
+	public Transform mageEnemy;
+	public Transform warriorEnemy;
+	public Transform nextFase;
+
 	private System.Random random;
+
+	private int[,] mapConfig = new int[,] {
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 3, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 4, 1},
+		{1, 0, 0, 0, 0, 1, 1, 1, 2, 1, 0, 1, 0, 0, 1, 0, 1, 1},
+		{1, 0, 0, 1, 0, 0, 2, 1, 4, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+		{1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 2, 1, 0, 0, 1, 0, 1},
+		{1, 0, 2, 4, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1},
+		{1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 8, 1, 2, 1},
+		{1, 0, 0, 0, 0, 1, 4, 2, 0, 1, 0, 0, 1, 1, 4, 2, 0, 1},
+		{1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 8, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};
 
 	// Use this for initialization
 	void Start ()
 	{
+		//should i remove this var??? Probably yes!
 		random = new System.Random();
-		GenerateMap();
+		//GenerateMapRandomly();
+		GenerateMapBasedOnProperties();
 	}
 
-	private void GenerateMap()
+	private void GenerateMapRandomly()
 	{
-		Debug.Log("Generating Map");
+		Debug.Log("Generating Map Randomly...");
 		Transform tile;
 		int tileNum;
 
@@ -42,7 +76,65 @@ public class MapGenerator : MonoBehaviour {
 				tiles.Add(newTile);
 			}
 		}
-		Debug.Log("Generating Map");
+		Debug.Log("Generating Map Complete");
+	}
+
+	private void GenerateMapBasedOnProperties()
+	{
+		Debug.Log("Generating Map based on properties...");
+
+		Vector2 mapDimension = new Vector2(mapConfig.GetLength(0), mapConfig.GetLength(1));
+
+		for (int x=0; x<mapDimension.x; x++) {
+			for (int y=0; y<mapDimension.y; y++) {
+				Vector3 tilePosition = new Vector3(-mapSize.y/2 + 0.5f + y, -mapDimension.x/2 + 0.5f + x, 0);
+				int objectCode = mapConfig[x,y];
+				InstantiateTile(objectCode, tilePosition);
+			}
+		}
+		Debug.Log("Generating Map Complete");
+	}
+
+	private void InstantiateTile(int objectCode, Vector3 tilePosition) {
+		Transform objectToInstantiate = communGround;
+		switch (objectCode) {
+			case TILE_COMMON_WALL:
+				objectToInstantiate = communWall;
+				break;
+			case TILE_DESTROYABLE_WALL:
+				objectToInstantiate = destroyableWall;
+				break;
+			default:
+				InstantiateObject(objectToInstantiate, tilePosition);
+				tilePosition.z = -1f;
+				switch(objectCode) {
+					case TILE_SPAWN_COMMON_ENEMY:
+						objectToInstantiate = commonEnemy;
+						break;
+					case TILE_SPAWN_MAGE_ENEMY:
+						objectToInstantiate = mageEnemy;
+						break;
+					case TILE_SPAWN_ROGUE_ENEMY:
+						objectToInstantiate = rogueEnemy;
+						break;
+					case TILE_SPAWN_WARRIOR_ENEMY:
+						objectToInstantiate = warriorEnemy;
+						break;
+					case TILE_SPAWN_PLAYER:
+						objectToInstantiate = player;
+						break;
+					default:
+						return;
+				}
+				break;
+		}
+		InstantiateObject(objectToInstantiate, tilePosition);
+	}
+
+	private void InstantiateObject (Transform objectToInstantiate, Vector3 position)
+	{
+		Transform newTile = Instantiate(objectToInstantiate, position, Quaternion.identity) as Transform;
+		tiles.Add(newTile);
 	}
 
 }
